f64965f5476c1fad0f0ecfedb540a5628ca76e2c
JINHYUP MOK <mokjh1117@gmail.com>
2024-07-16 16:53:10 +0900

    Time: 96 ms (32.09%), Space: 19.4 MB (86.50%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,6 +4,8 @@ class Solution:
         
         for word in strs:
             anagrams[''.join(sorted(word))].append(word)
+            # 문자열 정렬 : sorted(str)
+            # sorted(str) -> [s, t, r] 형태로 정렬되므로 ''.join(sorted(str))로 다시 합쳐주자!
             
         return list(anagrams.values())
         
