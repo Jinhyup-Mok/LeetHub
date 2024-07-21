3c4d2caacab32a50a5bd8def216a0875a54cdbc2
JINHYUP MOK <mokjh1117@gmail.com>
2024-07-21 19:19:51 +0900

    Update explain of split

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
@@ -17,6 +17,8 @@ class Solution {
         Collections.sort(letters, (s1,s2) -> {
             String[] part1 = s1.split(" ", 2); // [identifier, contents] 로 split
             String[] part2 = s2.split(" ", 2);
+            // split(split 기준, 배열 limit)
+            // split(" ", 2) : 공백을 기준으로 split 하되 split한 결과의 배열의 크기는 2로 제한 -> 즉, 첫번째로 등장한 공백만을 기준으로 split 된다 ex) ["hello", "my name is jinhyup"]
             
             // compareTo == 0 : 비교한 두 값이 같다! -> identifier 기준으로 정렬!
             if (part1[1].compareTo(part2[1]) == 0) return part1[0].compareTo(part2[0]);
